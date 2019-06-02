2b67684d71c6b10717735b032f2cb46a4c345757
zlldt <zlldtis@gmail.com>
2019-06-02 22:33:34 +0800

    Update gbfskillup.py

--- gbfskillup.py
+++ gbfskillup.py
@@ -1,20 +1,13 @@
+import numpy as np
 # 以1SR为单位
 # SSR升技，所需技狗为当前技能等级*2SR
-
+# SSR:SR = 10:1
+costbase = [10, 1]
 ssr_lvlup_cost = [2 * x for x in range(15)]
 ssr_lvlup_cost_v1 = [0 for x in range(15)]
 #detail数量 [SSR,SR]
 ssr_basecost_detail_v1 = [[0, 0] for x in range(15)]
 # 1SR等价1SR,1SSR等价10SR
-
-# basecost=[1,10]
-# bahamut_lvlup_cost = [10*x for x in range(15)]
-# ssr_basecost_detail=[[0,0] for x in range(15)]
-# bahamut_basecost_detail=[[0,0] for x in range(15)]
-# print(ssr_lvlup_cost_v1)
-# print(bahamut_lvlup_cost)
-# print(ssr_basecost_detail)
-# print(bahamut_basecost_detail)
 
 ssr_lvlup_cost_v1[1] = 10
 ssr_basecost_detail_v1[1] = [1, 0]
@@ -52,7 +45,7 @@ for x in range(2, 15):
         ssr_basecost_detail_v1[x][1] = ssr_basecost_detail_v1[x - 1][1] + 6
 print('SSR吃SR及SSR升技，获得x级技狗成本')
 print('ssr_lvlup_cost_v1',ssr_lvlup_cost_v1)
-print('ssr_lvlup_cost_v1',ssr_basecost_detail_v1)
+print('ssr_basecost_detail_v1',ssr_basecost_detail_v1)
 
 
 # 巴武，SSR天司，x技能等级吃合计x等级的SSR升技能
@@ -71,21 +64,27 @@ for x in range(5):
     bahamut_basecost_detail[x]=ssr_basecost_detail_v1[x]
 
 for x in range(5, 15):
-    value1 = bahamut_basecost_detail[1][0] * 10 + bahamut_basecost_detail[1][1]
-    value2 = bahamut_basecost_detail[x - 1][0] * 10 + bahamut_basecost_detail[x - 1][1]
+    #value1 = bahamut_basecost_detail[1][0] * 10 + bahamut_basecost_detail[1][1]
+    value1 = np.dot(bahamut_basecost_detail[1], costbase)
+    #value2 = bahamut_basecost_detail[x - 1][0] * 10 + bahamut_basecost_detail[x - 1][1]
+    value2 = np.dot(bahamut_basecost_detail[x - 1], costbase)
     maxcost = value1 + value2
     for y in range(1, x):
         if y <= (x - y):
-            value1 = bahamut_basecost_detail[y][0] * 10 + bahamut_basecost_detail[y][1]
-            value2 = bahamut_basecost_detail[x - y][0] * 10 + bahamut_basecost_detail[x - y][1]
+            #value1 = bahamut_basecost_detail[y][0] * 10 + bahamut_basecost_detail[y][1]
+            value1 = np.dot(bahamut_basecost_detail[y], costbase)
+            #value2 = bahamut_basecost_detail[x - y][0] * 10 + bahamut_basecost_detail[x - y][1]
+            value2 = np.dot(bahamut_basecost_detail[x - y], costbase)
             currentcost = value1 + value2
             # print('Bahamut lvl:',x,'PartA:lvl ',y,'cost: ',value1,'PartB:lvl ',x-y,'cost: ',value2,'currentcost=',value1+value2,bahamut_lvlup_cost[x],maxcost)
             if currentcost<maxcost:
             # if value1 + value2 < cost:
-                for z in range(6):
-                    bahamut_ssr_list[x][z] = bahamut_ssr_list[y][z] + bahamut_ssr_list[x - y][z]
-                bahamut_basecost_detail[x][0]=bahamut_basecost_detail[y][0]+bahamut_basecost_detail[x-y][0]
-                bahamut_basecost_detail[x][1]=bahamut_basecost_detail[y][1]+bahamut_basecost_detail[x-y][1]
+            #     for z in range(6):
+            #         bahamut_ssr_list[x][z] = bahamut_ssr_list[y][z] + bahamut_ssr_list[x - y][z]
+                bahamut_ssr_list[x] = np.add(bahamut_ssr_list[y],bahamut_ssr_list[x - y])
+                # bahamut_basecost_detail[x][0]=bahamut_basecost_detail[y][0]+bahamut_basecost_detail[x-y][0]
+                # bahamut_basecost_detail[x][1]=bahamut_basecost_detail[y][1]+bahamut_basecost_detail[x-y][1]
+                bahamut_basecost_detail[x] = np.add(bahamut_basecost_detail[y], bahamut_basecost_detail[x-y])
                 # bahamut_lvlup_cost[x]=bahamut_basecost_detail[x][0]*10+bahamut_basecost_detail[x][1]
                 maxcost = currentcost
             # print(x,bahamut_ssr_list[x],bahamut_basecost_detail[x])
@@ -102,4 +101,4 @@ print('巴武，SSR天司升技能所需成本：SSR技能武器数量：[0,SSRl
 print('bahamut_basecost_detail',bahamut_basecost_detail)
 print('升技能总成本：',bahamut_basecost_detail_sum)
 for x in range(1,15):
-    print('lv',x,'->lv',x+1,'所需',bahamut_ssr_list[x],'该级成本',bahamut_basecost_detail[x],'合计',bahamut_basecost_detail_sum[x])
+    print('lv',x,'->lv',x+1,'所需',bahamut_ssr_list[x],'成本',bahamut_basecost_detail[x],'合计',bahamut_basecost_detail_sum[x])
